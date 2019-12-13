884a9060a67b07fdbecdfb25e461b2de5434f53d
Andrei Ciprian Vrancianu <vrancianuandrei@gmail.com>
2019-12-13 13:44:21 +0200

    First

---
+++ GenericsList/GenericList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsList
+{
+    public class GenericList<T> where T : IComparable
+    {
+        // Constant Fields
+        private const int CapacityByDefault = 1;
+
+        private T[] array;
+
+        // Constructor
+        public GenericList(int capacity = CapacityByDefault)
+        {
+            this.Count = 0;
+            this.Capacity = capacity;
+
+            this.array = new T[capacity];
+        }
+
+        // Properties
+        public int Count { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.array[index];
+            }
+            set
+            {
+                this.array[index] = value;
+            }
+        }
+
+        // Adding an element
+        public void Add(T element)
+        {
+            this.Count++;
+            this.AutoGrow(this.Count);
+            this.array[this.Count - 1] = element;
+        }
+
+        // Accessing an element by index, 
+        public int IndexOf(T element)
+        {
+            return Array.IndexOf(this.array, element);
+        }
+
+        // Removing element by index
+        public void RemoveAt(int index)
+        {
+            this.Count--;
+            this.AutoGrow(this.Count);
+
+            Array.Copy(this.array, index + 1, this.array, index, this.Count - index);
+
+            this.array[this.Count] = default(T);
+        }
+
+        // Inserting an element at a given position 
+        public void Insert(int index, T element)
+        {         
+            this.Count++;
+            this.AutoGrow(this.Count);
+
+            Array.Copy(this.array, index, this.array, index + 1, this.Count - index - 1);
+
+            this.array[index] = element;
+        }
+
+        // Clearing the list
+        public void Clear()
+        {
+            this.Count = 0;
+            this.Capacity = CapacityByDefault;
+
+            this.array = new T[this.Capacity];
+        }
+
+        // Implement auto-grow functionality:
+        private void AutoGrow(int capacity)
+        {
+            if (capacity > this.Capacity)
+            {
+                this.Capacity *= 2;
+                Array.Resize(ref this.array, this.Capacity);
+            }
+        }
+    }
+}
---
+++ GenericsList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericsList;
+
+namespace GenericsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> arrayList = new GenericList<int>();
+
+            // Auto-grow and Add 
+            arrayList = new GenericList<int>(2);
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+
+            Console.WriteLine("Count: {0}", arrayList.Count);
+            Console.WriteLine("Capacity: {0}", arrayList.Capacity);
+
+            // Insert, RemoveAt
+            arrayList.Clear();
+
+            arrayList.Insert(0, 4);
+            arrayList.Insert(0, 3);
+            arrayList.Insert(0, 2);
+            arrayList.Insert(0, 1);
+
+            arrayList.RemoveAt(0);
+            arrayList.RemoveAt(arrayList.Count - 1);
+
+            Console.WriteLine("Count: {0}", arrayList.Count);
+            Console.WriteLine("Capacity: {0}", arrayList.Capacity);
+
+            // IndexOf
+            Console.WriteLine("Index of element 3: {0}", arrayList.IndexOf(3));
+
+            Console.ReadLine();
+        }
+    }
+}
